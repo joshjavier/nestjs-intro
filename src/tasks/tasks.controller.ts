634ecfef55a0b9d6dde48fcb03aa6791387c99f8
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-10-31 09:00:36 +0800

    done with sections 4 and 5

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -17,6 +17,7 @@ import { FindOneParams } from './find-one.params';
 import { UpdateTaskDto } from './update-task.dto';
 import { WrongTaskStatusException } from './exceptions/wrong-task-status.exception';
 import { Task } from './task.entity';
+import { CreateTaskLabelDto } from './create-task-label.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -68,6 +69,25 @@ export class TasksController {
   public async deleteTask(@Param() params: FindOneParams): Promise<void> {
     const task = await this.findOneOrFail(params.id);
     await this.tasksService.deleteTask(task);
+  }
+
+  @Post(':id/labels')
+  async addLabels(
+    @Param() { id }: FindOneParams,
+    @Body() labels: CreateTaskLabelDto[],
+  ): Promise<Task> {
+    const task = await this.findOneOrFail(id);
+    return this.tasksService.addLabels(task, labels);
+  }
+
+  @Delete(':id/labels')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async removeLabels(
+    @Param() { id }: FindOneParams,
+    @Body() labelNames: string[],
+  ): Promise<void> {
+    const task = await this.findOneOrFail(id);
+    await this.tasksService.removeLabels(task, labelNames);
   }
 
   private async findOneOrFail(id: string): Promise<Task> {
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -6,11 +6,16 @@ import { WrongTaskStatusException } from './exceptions/wrong-task-status.excepti
 import { Task } from './task.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CreateTaskLabelDto } from './create-task-label.dto';
+import { TaskLabel } from './task-label.entity';
 
 @Injectable()
 export class TasksService {
   constructor(
-    @InjectRepository(Task) private readonly tasksRepository: Repository<Task>,
+    @InjectRepository(Task)
+    private readonly tasksRepository: Repository<Task>,
+    @InjectRepository(TaskLabel)
+    private readonly labelRepository: Repository<TaskLabel>,
   ) {}
 
   async findAll(): Promise<Task[]> {
@@ -18,10 +23,16 @@ export class TasksService {
   }
 
   async findOne(id: string): Promise<Task | null> {
-    return this.tasksRepository.findOneBy({ id });
+    return this.tasksRepository.findOne({
+      where: { id },
+      relations: ['labels'],
+    });
   }
 
   async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    if (createTaskDto.labels) {
+      createTaskDto.labels = this.getUniqueLabels(createTaskDto.labels);
+    }
     return this.tasksRepository.save(createTaskDto);
   }
 
@@ -33,8 +44,40 @@ export class TasksService {
       throw new WrongTaskStatusException();
     }
 
+    if (updateTaskDto.labels) {
+      updateTaskDto.labels = this.getUniqueLabels(updateTaskDto.labels);
+    }
+
     Object.assign(task, updateTaskDto);
     return this.tasksRepository.save(task);
+  }
+
+  async removeLabels(task: Task, labelsToRemove: string[]): Promise<Task> {
+    task.labels = task.labels.filter(
+      (label) => !labelsToRemove.includes(label.name),
+    );
+    return this.tasksRepository.save(task);
+  }
+
+  async deleteTask(task: Task): Promise<void> {
+    await this.tasksRepository.remove(task);
+  }
+
+  public async addLabels(
+    task: Task,
+    labelDtos: CreateTaskLabelDto[],
+  ): Promise<Task> {
+    const names = new Set(task.labels.map((label) => label.name));
+    const labels = this.getUniqueLabels(labelDtos)
+      .filter((dto) => !names.has(dto.name))
+      .map((label) => this.labelRepository.create(label));
+
+    if (labels.length) {
+      task.labels = [...task.labels, ...labels];
+      return this.tasksRepository.save(task);
+    }
+
+    return task;
   }
 
   private isValidStatusTransition(
@@ -49,7 +92,10 @@ export class TasksService {
     return statusOrder.indexOf(currentStatus) <= statusOrder.indexOf(newStatus);
   }
 
-  async deleteTask(task: Task): Promise<void> {
-    await this.tasksRepository.delete(task);
+  private getUniqueLabels(
+    labelDtos: CreateTaskLabelDto[],
+  ): CreateTaskLabelDto[] {
+    const uniqueNames = [...new Set(labelDtos.map((label) => label.name))];
+    return uniqueNames.map((name) => ({ name }));
   }
 }
